9cf8f31d0ec7ed98786680a315a8b06ccf83b3c4
TheEnd99 <dossantosmatheus@ymail.com>
2013-11-04 16:39:42 -0200

    feat(caverna):Adiciona 3 tuneis provisorios

--- src/caverna.py
+++ src/caverna.py
@@ -34,6 +34,16 @@ class Caverna:
         self.main.style.backgroundImage = 'url(%s)' % CAVEX
         self.main.style.width = 1000
         self.main.style.height = 800
+        # criando um tunel
+        tunel = self.html.DIV()
+        tunel.setAttribute('style', 'height:700; width:33.33%;float: left;')
+        self.main <= tunel
+        tunel1 = self.html.DIV()
+        tunel1.setAttribute('style', 'height:700; width:33.33%;float: left;')
+        self.main <= tunel1
+        tunel2 = self.html.DIV()
+        tunel2.setAttribute('style', 'height:700; width:33.33%;float: left;')
+        self.main <= tunel2
         return self
 
 
